ce3e570180dee97a116861fc4538d91feb28c439
Sumithra <sumithra.maniselvan@techxle.com>
2024-09-16 19:25:11 +0530

    services resp

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router, Routes } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
@@ -13,18 +13,18 @@ throw new Error('Method not implemented.');
   @ViewChild('container', { read: ElementRef }) container!: ElementRef;
  
   cards = [
-    { title: 'Power Platform Solution', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/power.png',route: '/powerplatform', style:'top: 250px'},
-    { title: 'Cloud App Development', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/cloud.png',route: '/cloudapp', style:'top: 250px' },
-    { title: 'Staffing Service', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/Staffing.png',route: '/staffing', style:'top: 250px' },
-    { title: 'Full Stack Development', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/fullstack.png',route: '/fullstack', style:'top: 700px' },
-    { title: 'UI/UX Designing', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/UI.png',route: '/ui', style:'top: 700px' },
+    { title: 'Power Platform Solution', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/power.png',route: '/powerplatform'},
+    { title: 'Cloud App Development', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/cloud.png',route: '/cloudapp'},
+    { title: 'Staffing Service', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/Staffing.png',route: '/staffing'},
+    { title: 'Full Stack Development', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/fullstack.png',route: '/fullstack' },
+    { title: 'UI/UX Designing', content: 'The mechanism for implementing application lifecycle management (ALM) in Power Apps', symbol: 'assets/images/UI.png',route: '/ui' },
   ];
-  
+ 
   constructor(private router: Router){}
   navigateTo(Route: string) {
   this.router.navigate([`services/${Route}`]);
 }
 isHomeVisible: any;
 card: any;
-
+ 
 }
